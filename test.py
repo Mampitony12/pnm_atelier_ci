59df386ecd740d1b8b3fbf680c95f2ee8b7e71af
Pyrrex01 <tolojanahary.randriamanantsoa@student.passerellesnumeriques.org>
2025-08-01 17:27:10 +0300

    tes for convert_wallet function

--- test.py
+++ test.py
@@ -14,3 +14,9 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_convert_wallet():
+    obj = Wallet(0)
+    obj.set_balance(10)
+    assert obj.convert_wallet(5) == 50
+
